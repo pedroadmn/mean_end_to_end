16500d98e2f9397aad2050194fe7d14e16381f2e
Pedro Adelino de Mello Neto <pedroadmn@gmail.com>
2016-11-29 00:05:32 -0200

    Adding process env port

--- server.js
+++ server.js
@@ -91,6 +91,8 @@ app.use(morgan('dev'));
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(3000, function(){
-	console.log('Listening on port 3000');
+var port = process.ENV.PORT || 3000;
+
+app.listen(port, function(){
+	console.log('Listening on port ' + port);
 });
